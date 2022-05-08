e3c3862e3bf120c21b9f5dd725b7eeaba9f26ed6
Edson J Vargas <edson.jhair@yahoo.com>
2022-05-08 18:52:51 -0500

    EDIT: were onSubmit is applied
    onSubmit is changed from button to parent form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,24 @@
+import { useContext, useRef } from 'react';
+import { Context } from '../context/Context';
+
 export const SearchForm = () => {
+  const { setQuery } = useContext(Context);
+  const form = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(form.current);
+    const query = formData.get('input');
+    setQuery(query);
+  };
+
   return (
-    <form className='c-search-form' action=''>
+    <form
+      className='c-search-form'
+      action=''
+      ref={form}
+      onSubmit={(e) => handleSubmit(e)}
+    >
       <input
         type='text'
         name='input'
