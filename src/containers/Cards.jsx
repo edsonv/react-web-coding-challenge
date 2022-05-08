8aa5d1f6a34abb21b35274c22e52f0011132f8d4
Edson J Vargas <edson.jhair@yahoo.com>
2022-05-08 18:56:11 -0500

    EDIT: cleaning Cards component
    Changed axios for fetch API and other things

--- src/containers/Cards.jsx
+++ src/containers/Cards.jsx
@@ -1,31 +1,37 @@
-import axios from 'axios';
-import { useState, useEffect, useContext } from 'react';
+// import axios from 'axios';
+import { useEffect, useContext } from 'react';
 import { Card } from '../components/Card';
-import { Counter } from '../components/Counter';
 import { Loading } from '../components/Loading';
+import { Counter } from '../components/Counter';
 import { Pager } from '../components/Pager';
 import { Context } from '../context/Context';
 
 export const Cards = () => {
-  const { state } = useContext(Context);
-  const [data, setData] = useState([]);
+  const { state, setData } = useContext(Context);
 
   useEffect(() => {
-    axios
-      .get(
-        `https://bikeindex.org:443/api/v3/search?page=${state.currentPage}&per_page=10&location=Berlin&distance=100&stolenness=proximity`
-      )
+    fetch(
+      `https://bikeindex.org:443/api/v3/search?page=${
+        state.currentPage
+      }&per_page=10${
+        state.query ? `&query=${state.query}` : ''
+      }&location=Berlin&distance=100&stolenness=proximity`
+    )
       .then((response) => {
-        setData(response.data.bikes);
-      });
-  }, [state.currentPage]);
+        return response.json();
+      })
+      .then((data) => {
+        setData(data.bikes);
+      })
+      .catch((error) => console.error(error));
+  }, [state.query, state.currentPage]);
 
   return (
     <section className='c-cards-container'>
-      {data ? (
+      {state.data.length > 0 ? (
         <>
           <Counter />
-          {data.map((bike) => (
+          {state.data.map((bike) => (
             <Card bike={bike} key={bike.id} />
           ))}
           <Pager pageLimit={3} />
