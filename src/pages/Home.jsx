344a8d558a5b3a55c020b8fb5a8cd6300b0f5ef5
Edson J Vargas <edson.jhair@yahoo.com>
2022-05-09 00:59:35 -0500

    EDIT: to remove Header component and improve semantics

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
-import { Header } from '../components/Header';
 import { SearchForm } from '../components/SearchForm';
 import { Cards } from '../containers/Cards';
 
 export const Home = () => {
   return (
     <>
-      <Header />
-      <main>
-        <SearchForm />
-        <Cards />
-      </main>
+      <SearchForm />
+      <Cards />
     </>
   );
 };
