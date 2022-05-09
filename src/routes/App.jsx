41fb34b5bfb0eec9d56fbd22bb6dcd9723d97fd7
Edson J Vargas <edson.jhair@yahoo.com>
2022-05-09 01:00:06 -0500

    EDIT: added Layout component as a wrapper

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,18 +4,21 @@ import { Home } from '../pages/Home';
 import { Details } from '../pages/Details';
 import { Context } from '../context/Context';
 import { useInitialState } from '../hooks/useInitialState';
+import { Layout } from '../containers/Layout';
 
 export const App = () => {
   const initialState = useInitialState();
 
   return (
     <Context.Provider value={initialState}>
-      <HashRouter>
-        <Routes>
-          <Route index path='/' element={<Home />} />
-          <Route path='/details/:id' element={<Details />} />
-        </Routes>
-      </HashRouter>
+      <Layout>
+        <HashRouter>
+          <Routes>
+            <Route index path='/' element={<Home />} />
+            <Route path='/details/:id' element={<Details />} />
+          </Routes>
+        </HashRouter>
+      </Layout>
     </Context.Provider>
   );
 };
