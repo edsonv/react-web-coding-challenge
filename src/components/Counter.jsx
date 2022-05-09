ea3627c0d1dcdadceeb092454b2798478d1056bc
Edson J Vargas <edson.jhair@yahoo.com>
2022-05-09 00:53:58 -0500

    EDIT: state management and url
    Added local state management and fixed url

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,26 +1,28 @@
 // import axios from 'axios';
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { Context } from '../context/Context';
 
 export const Counter = () => {
-  const { state, setPages, setLoadingStatus } = useContext(Context);
+  const { state, setPages } = useContext(Context);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     fetch(
-      `https://bikeindex.org:443/api/v3/search/count?${
-        state.query ? `&query=${state.query}` : ''
-      }location=Berlin&distance=100&stolenness=proximity`
+      `https://bikeindex.org:443/api/v3/search/count?&query=${state.query}&location=Berlin&distance=100&stolenness=proximity`
     )
       .then((response) => {
         return response.json();
       })
-      .then(async (data) => {
-        await setPages(data.proximity);
-        setLoadingStatus(false);
-      })
-      .catch((error) => console.error(error));
-    console.log('Counter', state);
-  }, []);
+      .then((data) => {
+        setPages(data.proximity);
+        setLoading(false);
+      });
+  }, [state.query]);
 
-  return <p className='c-counter'>Total: {state.counter}</p>;
+  return (
+    <p className='c-counter'>
+      {loading ? 'Loading total...' : `Total: ${state.counter}`}
+    </p>
+  );
 };
